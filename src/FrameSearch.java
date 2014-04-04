7ebf7b70ce40969636b51d7f3008e22491964945
raffaelnagel <raffael.nagel@gmail.com>
2014-04-04 11:44:45 -0300

    Interface insert/load added commit

--- src/FrameSearch.java
+++ src/FrameSearch.java
@@ -1,7 +1,20 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 public class FrameSearch extends JFrame{
@@ -13,43 +26,126 @@ public class FrameSearch extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
 	Object [] columnNames = {"Pid","Name","Category","Brand","Leader"};
-	JTable mTable = new JTable(new DefaultTableModel(columnNames, 5));
+	JTable mTable = new JTable(new DefaultTableModel(columnNames, 0));
+	DefaultTableModel mModel = (DefaultTableModel) mTable.getModel();
+	JButton btnLoadData = new JButton("Load Data");
+	JButton btnInsertData = new JButton("Insert");
+	JLabel lbName = new JLabel(), lbCategory = new JLabel(), lbBrand = new JLabel(), lbLeader = new JLabel();
+	JTextField tfName = new JTextField(30), tfCategory = new JTextField(10), tfBrand = new JTextField(12), tfLeader = new JTextField(11);
 	
 		
 	public FrameSearch(){
 		
 		setTitle("Projects");
 		
-		setSize(500,500);		
-		setLocation(0,0);		
+		setSize(600,550);		
+		setLocationRelativeTo(null);		
 		setResizable(false);
+		setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		this.setVisible(true);
-		this.add(mTable);
 		
-		String URL = "jdbc:mysql://localhost:3306/Planning";
-		String login = "root";
-		String pass = "root";
+		JPanel firstPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel mainPanel = new JPanel(); 
+		JPanel buttonPanel = new JPanel();
+		JScrollPane scrollPane = new JScrollPane(mTable);
 		
-		DatabaseConnection mData = new DatabaseConnection(URL, login, pass);		
-		mData.openConnection();
-		List<Project> mListProject = mData.getProjectsFromDatabase();	
+		//
+		lbName.setText("Name: ");
+		lbCategory.setText("Category: ");
+		lbBrand.setText("Brand: ");
+		lbLeader.setText("Leader: ");
 		
-		int line = 0, column = 0;
-		for(Project p:mListProject){
-			column = 0;
-			mTable.setValueAt(p.getPid(), line, column);
-			column++;
-			mTable.setValueAt(p.getName(), line, column);
-			column++;
-			mTable.setValueAt(p.getCategory(), line, column);
-			column++;
-			mTable.setValueAt(p.getBrand(), line, column);
-			column++;
-			mTable.setValueAt(p.getLeader(), line, column);
-			line++;
-		}
+		firstPanel.add(lbName);
+		firstPanel.add(tfName);
+		firstPanel.add(lbCategory);
+		firstPanel.add(tfCategory);
+		firstPanel.add(lbBrand);
+		firstPanel.add(tfBrand);
+		firstPanel.add(lbLeader);
+		firstPanel.add(tfLeader);
+		//
+		
+		
+		mainPanel.setLayout(new BorderLayout());			
+		mainPanel.add(scrollPane, BorderLayout.SOUTH);
+		mainPanel.add(BorderLayout.CENTER,firstPanel);
+		DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
+		centerRender.setHorizontalAlignment(JLabel.CENTER);
+		
+		for(int i=0; i < mTable.getColumnCount(); i++)
+			mTable.getColumnModel().getColumn(i).setCellRenderer(centerRender);
+				
+		mTable.setCellSelectionEnabled(false);
+		mTable.setFillsViewportHeight(true);
+		
+		//button Load
+		btnLoadData.setPreferredSize(new Dimension(200,32));
+		btnLoadData.setVerticalTextPosition(AbstractButton.CENTER);
+		btnLoadData.setHorizontalTextPosition(AbstractButton.CENTER);
+		btnLoadData.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String URL = "jdbc:mysql://localhost:3306/Planning";
+				String login = "root";
+				String pass = "root";
+
+				DatabaseConnection mData = new DatabaseConnection(URL, login, pass);	
+				mData.openConnection();
+				List<Project> mListProject = mData.getProjectsFromDatabase();
+
+				for(int row = mModel.getRowCount() - 1; row >= 0 ; row--)
+					mModel.removeRow(row);
+				
+				int line = 0, column = 0;
+				for(Project p:mListProject){
+					mModel.addRow(new Object[]{});
+					column = 0;
+					mTable.setValueAt(p.getPid(), line, column);
+					column++;
+					mTable.setValueAt(p.getName(), line, column);
+					column++;
+					mTable.setValueAt(p.getCategory(), line, column);
+					column++;
+					mTable.setValueAt(p.getBrand(), line, column);
+					column++;
+					mTable.setValueAt(p.getLeader(), line, column);
+					line++;
+				}
+			}
+		});
+		
+		//button Insert
+				btnInsertData.setPreferredSize(new Dimension(200,32));
+				btnInsertData.setVerticalTextPosition(AbstractButton.CENTER);
+				btnInsertData.setHorizontalTextPosition(AbstractButton.CENTER);
+				btnInsertData.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						String URL = "jdbc:mysql://localhost:3306/Planning";
+						String login = "root";
+						String pass = "root";
+
+						DatabaseConnection mData = new DatabaseConnection(URL, login, pass);	
+						mData.openConnection();
+						
+						Project newProject = new Project();
+						newProject.setName(tfName.getText().toString());
+						newProject.setCategory(tfCategory.getText().toString());
+						newProject.setBrand(tfBrand.getText().toString());
+						newProject.setLeader(tfLeader.getText().toString());
+						
+						mData.updateProject(DatabaseConnection.SqlCommand.INSERT, newProject);						
+						JOptionPane.showInternalMessageDialog(getContentPane(), "Project Successfully Created.");
+						
+						btnLoadData.doClick();
+					}
+				});
+		buttonPanel.add(btnLoadData, BorderLayout.EAST);	
+		buttonPanel.add(btnInsertData, BorderLayout.WEST);
+		
+		//this.add(BorderLayout.NORTH, firstPanel);
+		this.add(BorderLayout.CENTER,mainPanel);
+		this.add(BorderLayout.SOUTH, buttonPanel);
 	}
 	
 }
--- src/Planning.java
+++ src/Planning.java
@@ -1,7 +1,4 @@
 
-
-import java.util.List;
-
 public class Planning {
 
 			
@@ -14,38 +11,18 @@ public class Planning {
 		DatabaseConnection mData = new DatabaseConnection(URL, login, pass);		
 		mData.openConnection();
 		
-		FrameSearch newFrame = new FrameSearch();
-		newFrame.show();
 		
-		//Login SystemLogin = new Login();
-		//SystemLogin.Show();
-		//boolean logged = SystemLogin.Log(mData);		
 		
-		/*
+		Login SystemLogin = new Login();
+		SystemLogin.Show();
+		boolean logged = SystemLogin.Log(mData);		
 		if(logged){
 		
-			mData.updateUser(DatabaseConnection.SqlCommand.DELETE, new User("Jojo","Jojoxx","123"));
-			
-			List<User> mResult = mData.getUsersFromDatabase("name","Jojo");			
-			if(mResult != null){
-				System.out.println("-- SQL RESULTS --");
-				for(User u:mResult){
-					System.out.println("Id: " + u.getId() + " Name: " + u.getName() + " Login: " + u.getLogin() + " Senha: " + u.getPassword());
-					System.out.println("----------------------------------------------");
-				}
-							
-			}	
-					
-					
-			Project p = new Project("Project 3", "BAU", "Pallmall", "RIL");
-			mData.updateProject(DatabaseConnection.SqlCommand.INSERT, p);
-			
-			List<Project> listP = mData.getProjectsFromDatabase();			
-			p = listP.get(listP.size()-1);
-			mData.updateProject(DatabaseConnection.SqlCommand.DELETE, p);
-			
+			FrameSearch newFrame = new FrameSearch();
+			newFrame.show();
+		
 		}
-		*/
+		
 		mData.closeConnection();		
 	}	
 }
